6779eb8d90a99cbd2f8ff7a7cc371d4f0e7e50f6
Riccardo Berto <riccardobrt@gmail.com>
2017-09-30 09:10:40 +0200

    connection pool

--- database.go
+++ database.go
@@ -11,8 +11,13 @@ const (
 )
 
 var r *redis.Client = redis.NewClient(&redis.Options{
-	Addr:     "127.0.0.1:6379",
-	Password: "",
-	DB:       2,
+	Addr:         "127.0.0.1:6379",
+	Password:     "",
+	DB:           2,
+	PoolSize:     4,
+	PoolTimeout:  30 * time.Seconds,
+	DialTimeout:  10 * time.Second,
+	ReadTimeout:  30 * time.Second,
+	WriteTimeout: 30 * time.Second,
 })
 var m sync.Mutex = sync.Mutex{}
